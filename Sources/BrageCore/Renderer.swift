4ced833bc435cebcc060f5893190fb8e1e9db9ed
Michael Enger <github@michaelenger.com>
2021-02-06 17:55:14 +0100

    Create renderer which can render the templates

---
+++ Sources/BrageCore/Renderer.swift
@@ -0,0 +1,175 @@
+/**
+*  Brage
+*  Copyright (c) Michael Enger 2021
+*  MIT license, see LICENSE file for details
+*/
+
+import Files
+import Ink
+import Stencil
+
+/// Renderer which can transform a template file into HTML content.
+public struct Renderer {
+    private let layoutTemplate: String
+    private let siteConfig: SiteConfig
+    private let sourceDirectory: Folder
+    private let stencilEnvironment: Environment
+    
+    /// Initialize the renderer with the specified source site directory.
+    ///
+    /// - Parameter source: Site directory to base the renderer on.
+    public init(source: Folder) throws {
+        self.sourceDirectory = source
+        
+        // Load the site config
+        let configString: String
+        if sourceDirectory.containsFile(named: "site.yaml") {
+            configString = try sourceDirectory.file(named: "site.yaml").readAsString()
+        } else if sourceDirectory.containsFile(named: "site.yml") {
+            configString = try sourceDirectory.file(named: "site.yml").readAsString()
+        } else {
+            throw RendererError.missingSiteConfig
+        }
+        
+        self.siteConfig = try SiteConfig.parse(from: configString)
+        
+        // Read the layout template
+        do {
+            self.layoutTemplate = try sourceDirectory.file(named: "layout.html").readAsString()
+        } catch is FilesError<LocationErrorReason> {
+            throw RendererError.missingLayoutTemplate
+        }
+        
+        // Construct render environment
+        do {
+            let templateDirectory = try sourceDirectory.subfolder(named:"templates")
+            let loader = MarkdownLoader(templateDirectory: templateDirectory)
+            stencilEnvironment = Environment(loader: loader)
+        } catch is FilesError<LocationErrorReason> {
+            // No templates for you
+            stencilEnvironment = Environment()
+        }
+    }
+    
+    /// Render a template file.
+    ///
+    /// - Parameter file: Template file to render.
+    /// - Parameter path: URI the page will be renderered at.
+    /// - Returns: HTML content.
+    public func render(file: File, uri: String = "/") throws -> String {
+        // Build data object
+        let rootPath = uri != "/"
+            ? String(repeating: "../", count: uri.count(of: Character("/")))
+            : "./"
+
+        let pageData = TemplateData(
+            site: TemplateSiteData(
+                title: siteConfig.title,
+                description: siteConfig.description,
+                image: siteConfig.image != nil ? "\(rootPath)assets/\(siteConfig.image!)" : nil,
+                root: rootPath,
+                assets: "\(rootPath)assets/"
+            ),
+            page: TemplatePageData(
+                title: file.nameExcludingExtension.titleified,
+                path: uri,
+                content: nil
+            ),
+            data: siteConfig.data
+        )
+        
+        // Render file
+        let pageContent: String
+
+        switch file.extension?.lowercased() {
+        case "markdown", "md":
+            pageContent = try renderMarkdownTemplate(from: file)
+        case "html":
+            let fileContents = try file.readAsString()
+            pageContent = try renderStencilTemplate(
+                environment: stencilEnvironment,
+                template: fileContents,
+                data: pageData
+            )
+        default:
+            throw RendererError.unrecognizedTemplate(file.name)
+        }
+        
+        let layoutData = TemplateData(
+            site: pageData.site,
+            page: TemplatePageData(
+                title: pageData.page.title,
+                path: uri,
+                content: pageContent
+            ),
+            data: pageData.data
+        )
+        return try renderStencilTemplate(
+            environment: stencilEnvironment,
+            template: layoutTemplate,
+            data: layoutData
+        )
+    }
+    
+    /// Render a markdown template from a specified file.
+    ///
+    /// - Parameter from: File to read and render markdown from.
+    /// - Returns: Rendered HTML content.
+    private func renderMarkdownTemplate(from file: File) throws -> String {
+        let fileContents = try file.readAsString()
+        let parser = MarkdownParser()
+        
+        return parser.html(from: fileContents)
+    }
+
+    /// Render a Stencil template from a specified file.
+    ///
+    /// - Parameter environment: Render environment to use.
+    /// - Parameter file: File to read and render from.
+    /// - Parameter data: Data to send to the template.
+    /// - Returns: Rendered HTML content.
+    private func renderStencilTemplate(environment: Environment, template: String, data: TemplateData) throws -> String {
+        return try environment.renderTemplate(string: template, context: [
+            "site": [
+                "title": data.site.title,
+                "description": data.site.description as Any,
+                "image": data.site.image as Any,
+                "root": data.site.root,
+                "assets": data.site.assets,
+            ],
+            "page": [
+                "title": data.page.title,
+                "path": data.page.path,
+                "content": data.page.content as Any,
+            ],
+            "data": data.data
+        ])
+    }
+}
+
+public enum RendererError: Error, Equatable {
+    case missingLayoutTemplate
+    case missingPagesDirectory
+    case missingSiteConfig
+    case unrecognizedTemplate(String)
+}
+
+public struct TemplateSiteData {
+    public let title: String
+    public let description: String?
+    public let image: String?
+    public let root: String
+    public let assets: String
+}
+
+public struct TemplatePageData {
+    public let title: String
+    public let path: String
+    public let content: String?
+}
+
+public struct TemplateData {
+    public let site: TemplateSiteData
+    public let page: TemplatePageData
+    public let data: [String: Any]
+}
---
+++ Tests/BrageTests/Tests/RendererTests.swift
@@ -0,0 +1,187 @@
+/**
+ *  Brage
+ *  Copyright (c) Michael Enger 2021
+ *  MIT license, see LICENSE file for details
+ */
+
+import Files
+import XCTest
+@testable import BrageCore
+
+final class RendererTests: XCTestCase {
+    private var sourceDirectory: Folder!
+
+    override func setUp() {
+        super.setUp()
+
+        // Create temporary directory
+        sourceDirectory = try! Folder.temporary.createSubfolderIfNeeded(withName: ".rendererTests")
+        try! sourceDirectory.empty()
+
+        // Fill it with some example data
+        try! sourceDirectory.createFile(
+            named: "site.yaml",
+            contents: Data("""
+            ---
+            title: Test Site
+            description: This is just a test.
+            image: lol.png
+            data:
+              text: A friendly little 🌲
+            """.utf8))
+        try! sourceDirectory.createFile(
+            named: "layout.html",
+            contents: Data("<title>{{site.title}}</title><body>{{page.content}}</body>".utf8))
+    }
+
+    override func tearDown() {
+        // Remove the temporary directory
+        try? sourceDirectory.delete()
+        super.tearDown()
+    }
+
+    func testBuildMissingSiteConfig() throws {
+        let siteConfigFile = try sourceDirectory.file(named: "site.yaml")
+        try siteConfigFile.delete()
+        
+        do {
+            _ = try Renderer(source: sourceDirectory)
+        } catch let e as RendererError {
+            XCTAssertEqual(e, RendererError.missingSiteConfig)
+        }
+    }
+
+    func testBuildMissingLayoutTemplate() throws {
+        let layoutFile = try sourceDirectory.file(named: "layout.html")
+        try layoutFile.delete()
+
+        do {
+            _ = try Renderer(source: sourceDirectory)
+        } catch let e as RendererError {
+            XCTAssertEqual(e, RendererError.missingLayoutTemplate)
+        }
+    }
+
+    func testRenderImportedTemplate() throws {
+        let templateFile = try sourceDirectory.createFile(
+            named: "index.html",
+            contents: Data("So Above {% include \"other.html\" %} So Below".utf8))
+
+        let templateDirectory = try sourceDirectory.createSubfolderIfNeeded(withName: "templates")
+        try templateDirectory.createFile(
+            named: "other.html",
+            contents: Data("--".utf8))
+
+        let renderer = try Renderer(source: sourceDirectory)
+        let result = try renderer.render(file: templateFile)
+        let expected = "<title>Test Site</title><body>So Above -- So Below</body>"
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testRenderMarkdownTemplate() throws {
+        let templateFile = try sourceDirectory.createFile(
+            named: "testpage.markdown",
+            contents: Data("THIS IS **TEST**".utf8))
+
+        let renderer = try Renderer(source: sourceDirectory)
+        let result = try renderer.render(file: templateFile)
+        let expected = "<title>Test Site</title><body><p>THIS IS <strong>TEST</strong></p></body>"
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testRenderStencilTemplate() throws {
+        let templateFile = try sourceDirectory.createFile(
+            named: "testpage.html",
+            contents: Data("THIS IS {{page.title}}".utf8))
+
+        let renderer = try Renderer(source: sourceDirectory)
+        let result = try renderer.render(file: templateFile)
+        let expected = "<title>Test Site</title><body>THIS IS Testpage</body>"
+
+        XCTAssertEqual(result, expected)
+    }
+
+    func testRenderUnknownTemplate() throws {
+        let templateFile = try sourceDirectory.createFile(
+            named: "testpage.lol",
+            contents: Data("THIS IS {{page.title}}".utf8))
+        
+        let renderer = try Renderer(source: sourceDirectory)
+
+        do {
+            _ = try renderer.render(file: templateFile)
+        } catch let e as RendererError {
+            XCTAssertEqual(e, RendererError.unrecognizedTemplate("testpage.lol"))
+        }
+    }
+
+    func testStencilVariables() throws {
+        let templateFile = try sourceDirectory.createFile(
+            named: "testpage.html",
+            contents: Data("""
+
+            {{site.title}}
+            {{site.description}}
+            {{site.image}}
+            {{site.root}}
+            {{site.assets}}
+            {{page.title}}
+            {{page.path}}
+            {{data.text}}
+
+            """.utf8))
+
+        let renderer = try Renderer(source: sourceDirectory)
+        let result = try renderer.render(file: templateFile)
+        let expected = """
+        <title>Test Site</title><body>
+        Test Site
+        This is just a test.
+        ./assets/lol.png
+        ./
+        ./assets/
+        Testpage
+        /
+        A friendly little 🌲
+        </body>
+        """
+
+        XCTAssertEqual(result, expected)
+    }
+    
+    func testStencilVariablesWithUri() throws {
+        let templateFile = try sourceDirectory.createFile(
+            named: "testpage.html",
+            contents: Data("""
+
+            {{site.title}}
+            {{site.description}}
+            {{site.image}}
+            {{site.root}}
+            {{site.assets}}
+            {{page.title}}
+            {{page.path}}
+            {{data.text}}
+
+            """.utf8))
+
+        let renderer = try Renderer(source: sourceDirectory)
+        let result = try renderer.render(file: templateFile, uri: "/some/test")
+        let expected = """
+        <title>Test Site</title><body>
+        Test Site
+        This is just a test.
+        ../../assets/lol.png
+        ../../
+        ../../assets/
+        Testpage
+        /some/test
+        A friendly little 🌲
+        </body>
+        """
+
+        XCTAssertEqual(result, expected)
+    }
+}
